b45554516b2d6798605079dae62b4ebf97e5a562
Ryan Deng <ryan.deng32@gmail.com>
2020-08-28 12:31:41 -0400

    finish work and projects section

--- src/components/work/Work.js
+++ src/components/work/Work.js
@@ -5,19 +5,27 @@ const Work = () => {
     <section className="work">
       <div className="work-title">Work</div>
       <div className="work-text">
-        <h1>Co-founder at Refinest</h1>
-        <ul>
+        <div className="work-text-title">Co-founder at Refinest</div>
+        <div className="work-text-date">2020.05 - 2020.08</div>
+
+        <ul className="work-text-list">
           <li>
             Worked in a team of two autonomously thoughout the work term
-            following the Agile workflow{" "}
+            following the <strong>Agile workflow</strong>
           </li>
           <li>
-            Researched the billion-dollar problem of “Creativity tools” in
-            partnership with the Problem Lab at the University of Waterloo
+            Researched the <strong>billion-dollar problem </strong>of
+            “Creativity tools” in partnership with the{" "}
+            <strong>Problem Lab</strong> at the{" "}
+            <strong>University of Waterloo</strong>
           </li>
           <li>
-            Scriped and designed the pitch deck and achieved finalist spot in
-            the Quantum Valley Investments Problem Pitch Competition.
+            Scriped and designed the pitch deck and achieved{" "}
+            <strong>finalist</strong> spot in the{" "}
+            <strong>
+              Quantum Valley Investments Problem Pitch Competition
+            </strong>
+            .
           </li>
         </ul>
       </div>
